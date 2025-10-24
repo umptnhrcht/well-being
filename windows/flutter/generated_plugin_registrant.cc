fcf69b67e72780821c4d457b650abb5aa4da8ff8
Mahaprasad Parida <mahaprasad923@gmail.com>
2025-10-24 21:11:05 +0530

    removed and added isar

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <isar_plus_flutter_libs/isar_plus_flutter_libs_plugin.h>
+#include <isar_flutter_libs/isar_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  IsarPlusFlutterLibsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("IsarPlusFlutterLibsPlugin"));
+  IsarFlutterLibsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("IsarFlutterLibsPlugin"));
 }
